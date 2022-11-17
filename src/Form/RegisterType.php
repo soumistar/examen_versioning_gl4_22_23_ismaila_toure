ec8edd6f17f7dbac54a2154d213711d1e7c5eae6
Hamidou KASSOGUE <hamidoukass@gmail.com>
2022-11-17 16:44:41 +0000

    Display of the register form with added properties and input style

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +16,39 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'placeholder' => 'Entrez votre adresse email'
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'attr' => [
+                    'placeholder' => 'Entrez votre mot de passe'
+                ]
+            ])
+            ->add('password_confirm', PasswordType::class, [
+                'label' => 'Confirmation du mot de passe',
+                'mapped' => false,
+                'attr' => [
+                    'placeholder' => 'Entrez encore votre mot de passe pour confirmer'
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre nom'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'S\'inscrire'
+            ])
         ;
     }
 
