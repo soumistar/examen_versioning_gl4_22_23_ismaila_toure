bd78e19b1e0915bc23c64326de8dd8808cb9a45c
Hamidou KASSOGUE <hamidoukass@gmail.com>
2022-11-18 15:13:40 +0000

    Hash of password

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
@@ -20,7 +21,7 @@ class RegisterController extends AbstractController
     }
 
     #[Route('/inscription', name: 'app_register')]
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordHasherInterface $encoder): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
@@ -30,6 +31,9 @@ class RegisterController extends AbstractController
         if($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
             //dd($user);
+
+            $password = $encoder->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
             
             //$doctrine = $this->getDoctrine()->getManager();
             //$doctrine->persist($user);
