ea127f8ff3b7f4bfe65d019e26fc1c0a90463985
Hamidou KASSOGUE <hamidoukass@gmail.com>
2022-11-22 00:48:08 +0000

    Making password change page 2

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -6,9 +6,20 @@ use App\Form\ChangePasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class AccountController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/compte', name: 'app_account')]
     public function index(): Response
     {
@@ -16,12 +27,31 @@ class AccountController extends AbstractController
     }
 
     #[Route('/compte/changer-mot-de-passe', name: 'app_account_password')]
-    public function password(): Response
-    {
+    public function password(Request $request, UserPasswordHasherInterface $encoder): Response
+    {       
+        $notification = null;
         $user = $this->getUser();
         $form = $this->createForm(ChangePasswordType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $old_pwd = $form->get('old_password')->getData();
+            if($encoder->isPasswordValid($user, $old_pwd)) {
+                $new_pwd = $form->get('new_password')->getData();
+                $password = $encoder->hashPassword($user, $new_pwd);
+                $user->setPassword($password);
+                $this->entityManager->flush();
+                $notification = 'Votre mot de passe a été changé avec succès.';
+            }
+            else {
+                $notification = 'Votre mot de passe actuel n\'est pas le bon';
+            }
+        }
+
         return $this->render('account/password.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'notification' => $notification
         ]);
     }
 }
