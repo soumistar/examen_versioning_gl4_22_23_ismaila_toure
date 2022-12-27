efd893eef59f62c4d741e073fddd77d46a93aee9
Hamidou KASSOGUE <hamidoukass@gmail.com>
2022-12-27 15:27:46 +0000

    Update of entity user with admin making

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -5,8 +5,12 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
@@ -20,12 +24,20 @@ class UserCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id')->hideWhenCreating(),
+            IdField::new('id')->hideOnForm(),
             TextField::new('email'),
-            TextField::new('password')->setFormType(PasswordType::class),
+            TextField::new('password')->setFormType(PasswordType::class)->onlyWhenCreating(),
             TextField::new('firstname'),
             TextField::new('lastname'),
+            ImageField::new('avatar')
+                ->setBasePath('upload/images/users')
+                ->setUploadDir('public/upload/images/users')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setSortable(false),
+            BooleanField::new('active'),
+            CollectionField::new('roles')->hideOnForm(),
             DateTimeField::new('created_at'),
+            DateTimeField::new('login_at')->hideOnForm()
         ];
     }
 
