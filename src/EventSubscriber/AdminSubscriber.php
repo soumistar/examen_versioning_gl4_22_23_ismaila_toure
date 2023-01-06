565727150767c8324f1b0a289094b38b4f6a3311
Hamidou KASSOGUE <hamidoukass@gmail.com>
2023-01-06 16:32:03 +0000

    Update of event subscriber for created_at field for all entities

--- src/EventSubscriber/AdminSubscriber.php
+++ src/EventSubscriber/AdminSubscriber.php
@@ -1,8 +1,14 @@
 <?php
 namespace App\EventSubscriber;
 
+use App\Entity\Arrival;
 use App\Entity\Category;
+use App\Entity\Customer;
+use App\Entity\Like;
+use App\Entity\Photo;
 use App\Entity\Product;
+use App\Entity\Review;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -19,7 +25,15 @@ class AdminSubscriber implements EventSubscriberInterface
     {
         $entityInstance = $event->getEntityInstance();
 
-        //if (!($entityInstance instanceof Product) && !($entityInstance instanceof Category)) return;
+        if (!($entityInstance instanceof Product) 
+            && !($entityInstance instanceof Category)
+            && !($entityInstance instanceof Photo)
+            && !($entityInstance instanceof User)
+            && !($entityInstance instanceof Customer)
+            && !($entityInstance instanceof Like)
+            && !($entityInstance instanceof Review)
+            && !($entityInstance instanceof Arrival)) return;
+        
         if(!$entityInstance->getCreatedAt()) $entityInstance->setCreatedAt(new \DateTimeImmutable);
     }
 }
